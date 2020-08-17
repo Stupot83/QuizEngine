c2cbd61765b05a6790c912aef9c71a92a1996024
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-08-17 10:51:21 +0100

    :art: Format existing files :art:

--- database/connection.js
+++ database/connection.js
@@ -1,3 +1,3 @@
 module.exports = {
- mongoURI: "mongodb+srv://Stupot83:Corjb2021x@quizengine1.grwfs.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://Stupot83:Corjb2021x@quizengine1.grwfs.mongodb.net/<dbname>?retryWrites=true&w=majority"
 };
