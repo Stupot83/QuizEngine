076fdcd6425fa093a855474ee78ac4e1b0fb66ee
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-08-17 16:42:25 +0100

    :arrow_up: Install further dependencies and setup font and sass :arrow_up:

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>QuizEngine Placeholder</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>QuizEngine Placeholder</h1>
+        </div>
+    );
 }
 
 export default App;
