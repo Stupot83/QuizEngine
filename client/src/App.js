cc22aaf87d7d82dfe0162858e810f92ea64183af
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-08-18 22:34:43 +0100

    :construction: Add basic Home page and Authorized Route WIP :construction:

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Topnav from "./components/navigation/Topnav";
 import Landing from "./components/Landing";
 import Registration from "./components/entry/Registration";
 import Login from "./components/entry/Login";
+import Home from "./components/Home";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                     <Route exact path="/" component={Landing} />
                     <Route exact path="/register" component={Registration} />
                     <Route exact path="/login" component={Login} />
+                    <Route exact path="/home" component={Home} />
                 </div>
             </Router>
         </Provider>
