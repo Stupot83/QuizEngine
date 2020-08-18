b458ff0d2cfddd64940bc81a8e8cf3b07824d6b2
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-08-18 10:13:52 +0100

    :construction: Add basic navbar for now ongoing WIP :construction:

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Topnav from "./components/navigation/Topnav";
 import Landing from "./components/Landing";
 
 function App() {
     return (
       <Router>
       <div className="App">
+              <Topnav />
               <Route exact path="/" component={Landing} />
       </div>
   </Router>
