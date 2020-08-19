9a1a929aacee81834c85de47f193e488ad9e1c16
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-08-19 12:02:34 +0100

    :construction: Setup Quiz API in Routes :construction:

--- routes/api/users.js
+++ routes/api/users.js
@@ -42,49 +42,49 @@ router.post("/register", (req, res) => {
 
 // POST user/login - Login a User and return JWT
 router.post("/login", (req, res) => {
- // Input from login form is validated
- const { invalidEntries, isValid } = validationForLogin(req.body);
- // Check validation
- if (!isValid) {
-     return res.status(400).json(invalidEntries);
- }
- const email = req.body.email;
- const password = req.body.password;
- // Check the Mongo Database in the Atlas Cluster for User details
- User.findOne({ email }).then(user => {
-     // Check if User exists already in Mongo Database in Atlas Cluster
-     if (!user) {
-         return res.status(404).json({ emailnotfound: "User with this email does not exist!" });
-     }
-     // Check the password
-     bcrypt.compare(password, user.password).then(isMatch => {
-         if (isMatch) {
-             // User details match those in Mongo Database in Atlas Cluster
-             // Create JWT Payload
-             const payload = {
-                 id: user.id,
-                 name: user.name,
-                 email: user.email
-             };
-             // Sign the token
-             jwt.sign(
-                 payload,
-                 connection.secretOrKey,
-                 {
-                     expiresIn: 10800 // 3 hours in seconds
-                 },
-                 (err, token) => {
-                     res.json({
-                         success: true,
-                         token: "Bearer " + token
-                     });
-                 }
-             );
-         } else {
-             return res.status(400).json({ passwordincorrect: "Password entered is incorrect!" });
-         }
-     });
- });
+    // Input from login form is validated
+    const { invalidEntries, isValid } = validationForLogin(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(invalidEntries);
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    // Check the Mongo Database in the Atlas Cluster for User details
+    User.findOne({ email }).then(user => {
+        // Check if User exists already in Mongo Database in Atlas Cluster
+        if (!user) {
+            return res.status(404).json({ emailnotfound: "User with this email does not exist!" });
+        }
+        // Check the password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // User details match those in Mongo Database in Atlas Cluster
+                // Create JWT Payload
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                };
+                // Sign the token
+                jwt.sign(
+                    payload,
+                    connection.secretOrKey,
+                    {
+                        expiresIn: 10800 // 3 hours in seconds
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res.status(400).json({ passwordincorrect: "Password entered is incorrect!" });
+            }
+        });
+    });
 });
 
 module.exports = router;
